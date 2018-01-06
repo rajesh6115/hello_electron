10bed3840f72f58044c7fe053369ba25b75fa187
Rajesh <sahoorajesh.d@gmail.com>
2018-01-06 11:21:44 +0530

    [hello] made hello running app. To run just type electron .

---
+++ main.js
@@ -0,0 +1,20 @@
+// for business logic
+const electron = require('electron')
+const app = electron.app
+const BrowserWindow = electron.BrowserWindow
+
+
+let mainWindow
+
+app.on('ready', ()=>{
+    mainWindow = new BrowserWindow({
+        height: 600,
+        width: 800
+    });
+
+    mainWindow.loadURL("file://"+__dirname+"/index.html");
+
+    mainWindow.on('closed', function(){
+        mainWindow = null;
+    });
+});
